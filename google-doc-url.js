944e7633d24cdb1980529bc8d69c1b5a5eeebca7
Pratichhya <39898768+Pratichhya@users.noreply.github.com>
2020-12-29 18:30:38 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_0aWBg99tQQVf0tCkqlXggdVwaSTorG0H9uQizXVSgc/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyB-LYEC7FayrCJTqNwPAzQlvi4pKpmu17E';
